8540072cd3bef42abd4126965515078bd4bfd63d
bulatgimazov <gimazovbulat@gmail.com>
2021-12-19 22:44:46 +0300

    fixed 5th task

--- queuing_system/src/main/java/ru/itis/queuing_system/FifthTask.java
+++ queuing_system/src/main/java/ru/itis/queuing_system/FifthTask.java
@@ -1,13 +1,15 @@
 package ru.itis.queuing_system;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.MutablePair;
 
 import java.sql.Timestamp;
 import java.time.Month;
 import java.time.format.TextStyle;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -15,29 +17,30 @@ public class FifthTask extends AbstractTask {
 
     @Override
     public void avgAndDispersion(List<Timestamp> dates) {
-        AtomicReference<Map<ImmutablePair<Month, Integer>, Long>> monthAndApplicationAmount = new AtomicReference<>();
-        Arrays.asList(Month.values()).forEach(month -> {
-            monthAndApplicationAmount.set(dates.stream()
-                    .map(Timestamp::toLocalDateTime)
-                    .filter(t -> t.getMonth() == month)
-                    .collect(Collectors.groupingBy(date -> new ImmutablePair<>(date.getMonth(), date.getYear()), Collectors.counting())));
+        Map<MutablePair<Month, Integer>, Long> monthAndApplicationAmount = (dates.stream()
+                .map(Timestamp::toLocalDateTime)
+                .sorted()
+                .collect(Collectors.groupingBy(date -> new MutablePair<>(date.getMonth(), date.getYear()), Collectors.counting())));
+
+        Map<Month, Integer> monthAndAmountInList = new EnumMap<>(Month.class);
+        for (Month m : Month.values()){
+            monthAndAmountInList.putIfAbsent(m, 0);
+        }
+
+        monthAndApplicationAmount.forEach((key, value) -> {
+            Month month = key.getKey();
+            monthAndAmountInList.put(month, monthAndAmountInList.getOrDefault(month, 0) + 1);
+            key.setValue(monthAndAmountInList.get(month));
         });
 
-        Integer earliestYear = monthAndApplicationAmount.get().keySet().stream()
-                .min(Comparator.comparingInt(ImmutablePair::getValue))
-                .map(ImmutablePair::getValue)
-                .orElseThrow(() -> new IllegalStateException("something went wrong"));
-
-        monthAndApplicationAmount.get().forEach((key, value) -> key.setValue(key.getValue() - earliestYear + 1));
-
-        monthAndApplicationAmount.get().forEach((key, value) -> log.info(
+        monthAndApplicationAmount.forEach((key, value) -> log.info(
                 "Месяц {} № {}, кол-во заявок {}",
                 key.getKey().getDisplayName(TextStyle.FULL, Locale.ROOT),
                 key.getValue(),
                 value
         ));
 
-        getDispersion(getAverage(monthAndApplicationAmount.get()), monthAndApplicationAmount.get());
+        getDispersion(getAverage(monthAndApplicationAmount), monthAndApplicationAmount);
     }
 
     @Override
